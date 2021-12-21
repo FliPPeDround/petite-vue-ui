c9f05fca4e342cc8b471274289e369978148179c
jojoypeng <jojoypeng@tencent.com>
2021-12-21 17:57:44 +0800

    feat:完善dialog属性

--- src/components/dialog/getDialogAttr.ts
+++ src/components/dialog/getDialogAttr.ts
@@ -1,12 +1,21 @@
 export const getDialogAttr = () => {
   const pvDialogAttr = document.getElementsByTagName('pv-dialog')
   for (let i = 0; i < pvDialogAttr.length; i++) {
-    // const text = pvDialogAttr?.[i].textContent || undefined;
-    const bodySlot = pvDialogAttr?.[i].children?.['body' as unknown as number].outerHTML.slice(20, -11).replace(/[\r\n]/g, "")
-    const visible = pvDialogAttr?.[i].attributes?.['visible' as unknown as number].value
+    const bodySlot = pvDialogAttr?.[i].children?.['body' as unknown as number]?.outerHTML.slice(20, -11).replace(/[\r\n]/g, "")
+    const visible = pvDialogAttr?.[i].attributes?.['visible' as unknown as number]?.value
+    const title = pvDialogAttr?.[i].attributes?.['title' as unknown as number]?.value
+    const width = pvDialogAttr?.[i].attributes?.['width' as unknown as number]?.value
+    const top = pvDialogAttr?.[i].attributes?.['top' as unknown as number]?.value
+    const closeOnClickModal = pvDialogAttr?.[i].attributes?.['closeOnClickModal' as unknown as number]?.value
+    const showClose = pvDialogAttr?.[i].attributes?.['showClose' as unknown as number]?.value
     pvDialogAttr?.[i].setAttribute('v-scope', `pvDialog({
       ${bodySlot ? "bodySlot:'"+ bodySlot + "'," :''}
       ${visible ? "visible:'"+ visible + "'," :''}
+      ${title ? "title:'"+ title + "'," :''}
+      ${width ? "width:'"+ width + "'," :''}
+      ${top ? "top:'"+ top + "'," :''}
+      ${closeOnClickModal ? "closeOnClickModal:'"+ closeOnClickModal + "'," :''}
+      ${showClose ? "showClose:'"+ showClose + "'," :''}
     })`)
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,44 @@
 import { createApp, reactive } from 'petite-vue'
 import { pvButton, pvDialog, pvInput, pvSwitch } from './components'
 
-const store = reactive({
-  dialogShow() {
-    this.visible = true
-  },
-  handleChange() {
-    console.log(123)
-  },
-  switchOn: true,
-  visible: false,
-  btn: pvButton({
-    slot: '按钮',
-    type: 'info',
-    click: 'store.dialogShow()'
-  }),
-  dialog: pvDialog({
-    visible: 'store.visible',
-    bodySlot: '你好呀！'
-  }),
-  input: pvInput({}),
-  switch: pvSwitch({
-    Vmodel: 'store.switchOn'
-  })
+// const store = reactive({
+//   dialogShow() {
+//     this.visible = true
+//   },
+//   handleChange() {
+//     console.log(123)
+//   },
+//   switchOn: true,
+//   visible: false,
+//   btn: pvButton({
+//     slot: '按钮',
+//     type: 'info',
+//     click: 'store.dialogShow()'
+//   }),
+//   dialog: pvDialog({
+//     visible: 'store.visible',
+//     bodySlot: '你好呀！'
+//   }),
+//   input: pvInput({}),
+//   switch: pvSwitch({
+//     Vmodel: 'store.switchOn'
+//   })
+// })
+// const printA = () => {
+//   console.log('A')
+// }
+let visible = reactive({
+  value: false
 })
-const printA = () => {
-  console.log('A')
+const opendialog = () => {
+  visible.value = true
 }
+
 
 createApp({
   // store,
   pvButton,
-  printA,
-  pvDialog
+  opendialog,
+  pvDialog,
+  visible
 }).mount()
